dabab98f51beaf51d538719c486e3bf68c9bd6b6
sankeerthana123 <saisankeerthana2003@gmail.com>
2022-11-22 10:49:49 +0530

    Demo for Team

---
+++ src/main/java/com/se/se/App.java
@@ -0,0 +1,13 @@
+package com.se.se;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
